c3ed12ed02b5400f5b483188c9726daac24ca1a0
Gregory Kahn <gkahn13@gmail.com>
2013-06-14 08:48:35 -0700

    Added locks

--- scripts/ImageDetection.py
+++ scripts/ImageDetection.py
@@ -8,19 +8,23 @@ import sys
 from geometry_msgs.msg import Twist, PointStamped
 import tf
 
+from threading import *
+
 class ImageDetectionClass():
       def __init__(self):
             #PointStamped list
             self.cancerPoints = []
-
-            #rospy.init_node('image_detect', anonymous=True)
+            #Lock so two arms can access one ImageDetectionClass
+            self.lock = Lock()
 
             rospy.Subscriber('stereo_points_3d', PointStamped, self.stereoCallback)
 
             #rospy.spin()
 
       def stereoCallback(self, msg):
+            self.lock.acquire()
             self.cancerPoints.append(msg)
+            self.lock.release()
 
       def hasFoundCancer(self):
             return len(self.cancerPoints) > 0
@@ -29,9 +33,11 @@ class ImageDetectionClass():
             if not self.hasFoundCancer():
                   return None
 
+            self.lock.acquire()
             cancerPoint = self.cancerPoints[-1]
             self.cancerPoints = self.cancerPoints[:-1]
-            
+            self.lock.release()
+
             return cancerPoint
 
       
