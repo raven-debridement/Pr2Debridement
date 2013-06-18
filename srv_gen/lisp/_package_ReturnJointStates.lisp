216f7ca01bc4e99fd5fdee5ddcc0a3046262b4a7
Gregory Kahn <gkahn13@gmail.com>
2013-06-17 18:16:46 -0700

    Working on ArmControl, which will replace CommandPose and CommandTwist

---
+++ srv_gen/lisp/_package_ReturnJointStates.lisp
@@ -0,0 +1,12 @@
+(cl:in-package Pr2Debridement-srv)
+(cl:export '(NAME-VAL
+          NAME
+          FOUND-VAL
+          FOUND
+          POSITION-VAL
+          POSITION
+          VELOCITY-VAL
+          VELOCITY
+          EFFORT-VAL
+          EFFORT
+))
